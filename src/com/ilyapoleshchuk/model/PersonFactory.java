8fa4d40a6c529a95773410f9f3f7ed2498611671
Nikita Maslov <nikita.maslov.develop@gmail.com>
2019-04-14 19:18:42 +0300

    Fix visibility modifiers

--- src/com/ilyapoleshchuk/model/PersonFactory.java
+++ src/com/ilyapoleshchuk/model/PersonFactory.java
@@ -2,11 +2,17 @@ package com.ilyapoleshchuk.model;
 
 public class PersonFactory {
 
-    static Person createPerson(String firstName, String lastName, Generation generation) {
+    private PersonFactory() {
+    }
+
+    public static Person createPerson(String firstName, String lastName, Generation generation) {
         switch (generation) {
-            case NEW_SCHOOL: return new NewSchoolPerson(firstName, lastName);
-            case OLD_SCHOOL: return new OldSchoolPerson(firstName, lastName);
-            default: throw new IllegalArgumentException();
+            case NEW_SCHOOL:
+                return new NewSchoolPerson(firstName, lastName);
+            case OLD_SCHOOL:
+                return new OldSchoolPerson(firstName, lastName);
+            default:
+                throw new IllegalArgumentException();
         }
     }
 }
