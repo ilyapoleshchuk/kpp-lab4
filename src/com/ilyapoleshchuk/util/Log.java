e68e9bb601f869bdbd7142f4f1f2ecf2cfe7c88c
Nikita Maslov <nikita.maslov.develop@gmail.com>
2019-04-14 18:53:32 +0300

    Implement logger

--- src/com/ilyapoleshchuk/util/Log.java
+++ src/com/ilyapoleshchuk/util/Log.java
@@ -1,4 +1,26 @@
 package com.ilyapoleshchuk.util;
 
 public class Log {
+
+    private enum Level {
+        INFO,
+        ERROR
+    }
+
+    public static void i(String message) {
+        log(Level.INFO, message);
+    }
+
+    public static void e(String message) {
+        log(Level.ERROR, message);
+    }
+
+    private static void log(Level level, String message) {
+        String prettyMessage = createPrettyMessage(level, message);
+        System.out.println(prettyMessage);
+    }
+
+    private static String createPrettyMessage(Level level, String message) {
+        return "[" + level.toString() + "]: " + message;
+    }
 }
